41064a84826b3a6ae33c23d925f6147b5853b6be
asdsajt <egyed.vince1998@gmail.com>
2021-08-16 10:11:22 +0200

    config after flashing

---
+++ scripts/config_after_flash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl -y
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt install ros-melodic-desktop-full -y
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+sudo rosdep init
+rosdep update
+
+sudo apt-get install ros-melodic-catkin -y
+
+apt-get install python-catkin-tools -y
+
+sudo apt-get install can-utils -y
